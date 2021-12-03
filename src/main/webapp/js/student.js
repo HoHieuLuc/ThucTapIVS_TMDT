bd290f78ee47319bcff129b5c13a9a529eccace1
Ho Hieu Luc <kingofika123@gmail.com>
2021-12-03 21:43:50 +0700

    Cập nhật thêm ví dụ để sau này làm

--- src/main/webapp/js/student.js
+++ src/main/webapp/js/student.js
@@ -1,13 +1,30 @@
 const formDOM = document.querySelector('#create-student-form');
+const testBtnDOM = document.querySelector('#test-btn');
+const errorMsgDOM = document.querySelector('#error-msg');
 
 formDOM.addEventListener('submit', async (event) => {
     event.preventDefault();
-    const data = new FormData(formDOM);
-
+    const formData = new FormData(formDOM);
+    errorMsgDOM.textContent = '';
     try {
-        await axios.post('CreateStudent', data);
-        window.location.href = './';
+        const { data } = await axios.post('../api/v1/student/create', formData);
+        window.location.href = './index';
+        console.log(data);
         //console.log('adadad');
+    } catch (error) {
+        errorMsgDOM.textContent = error.response.data.message;
+        //console.log(error.response.data.message);
+    }
+});
+
+testBtnDOM.addEventListener('click', async (event) => {
+    const formData = new FormData(formDOM);
+    /* const json = JSON.stringify(Object.fromEntries(formData));
+    const jsonData = new FormData();
+    jsonData.append('jsonStudent', json); */
+    try {
+        const { data } = await axios.post('../api/v1/student/update', formData);
+        console.log(data);
     } catch (error) {
         console.log(error);
     }
