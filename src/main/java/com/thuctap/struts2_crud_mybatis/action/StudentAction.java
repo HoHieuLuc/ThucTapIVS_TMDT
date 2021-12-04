05278b7090d18c43302fee750399b7bd65dd157e
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2021-12-04 15:56:50 +0700

    Thêm filter hỗ trợ tiếng việt full

--- src/main/java/com/thuctap/struts2_crud_mybatis/action/StudentAction.java
+++ src/main/java/com/thuctap/struts2_crud_mybatis/action/StudentAction.java
@@ -3,24 +3,16 @@ package com.thuctap.struts2_crud_mybatis.action;
 import java.io.Reader;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
-import java.io.PrintWriter;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Update;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.*;
 
-import com.google.gson.Gson;
-import com.opensymphony.xwork2.ActionSupport;
 import com.thuctap.struts2_crud_mybatis.model.Student;
+import com.opensymphony.xwork2.ActionSupport;
 
 import mybatis.mapper.StudentMapper;
 
@@ -37,16 +29,12 @@ public class StudentAction extends ActionSupport {
         this.listStudents = listStudents;
     }
 
-    @Action(value = "/student/index", results = { @Result(location = "/index.html") })
-    public String listStudent() {
-        HttpServletResponse response = ServletActionContext.getResponse();
-        response.setCharacterEncoding("utf-8");
-        return SUCCESS;
-    }
+    @Override
+    @Action(value = "/index", results = { @Result(location = "/index.jsp") })
+    public String execute() throws IOException {
 
-    @Action(value = "/api/v1/student/list", results = { @Result(location = "/index.html") })
-    public String viewStudents() throws IOException {
-
+        // Đọc file xml để khởi tạo kết nối đến mysql server và cấu hình interface
+        // studentmapper
         Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
         // Mở Session
@@ -59,27 +47,17 @@ public class StudentAction extends ActionSupport {
         // Lấy dữ liệu toàn bộ học sinh
         listStudents = studentMapper.getAll();
 
-        // chuyển danh sách học sinh sang json
-        Gson gson = new Gson();
-        String json = gson.toJson(listStudents);
+        // Gửi kết quả truy vấn đến view
+        setListStudents(listStudents);
 
-        // trả về kết quả là json
-        HttpServletResponse response = ServletActionContext.getResponse();
-        response.setCharacterEncoding("utf-8");
-        response.setContentType("application/json;charset=utf-8");
-        response.setHeader("Cache-Control", "no-cache");
-        PrintWriter pw = response.getWriter();
-        pw.print(json);
-        pw.flush();
-        pw.close();
-
-        // System.out.println(json);
+        // ngắt kết nối
+        session.close();
 
         return SUCCESS;
     }
 
     /*  */
-    // region getters setters
+
     private int id;
     private String name;
     private String branch;
@@ -87,166 +65,78 @@ public class StudentAction extends ActionSupport {
     private int phone;
     private String email;
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getBranch() {
-        return branch;
-    }
+	public String getBranch() {
+		return branch;
+	}
 
-    public void setBranch(String branch) {
-        this.branch = branch;
-    }
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
 
-    public int getPercentage() {
-        return percentage;
-    }
+	public int getPercentage() {
+		return percentage;
+	}
 
-    public void setPercentage(int percentage) {
-        this.percentage = percentage;
-    }
+	public void setPercentage(int percentage) {
+		this.percentage = percentage;
+	}
 
-    public int getPhone() {
-        return phone;
-    }
+	public int getPhone() {
+		return phone;
+	}
 
-    public void setPhone(int phone) {
-        this.phone = phone;
-    }
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    // endregion
-
-    // validate đơn giản
-    public boolean isValid() {
-        return name != null && name.length() > 0 && branch != null && branch.length() > 0
-                && percentage > 0 && phone > 0 && email != null && email.length() > 0;
-    }
-
-    @Action(value = "/student/create", results = { @Result(location = "/create.html") })
-    public String viewCreateStudent() {
-        HttpServletResponse response = ServletActionContext.getResponse();
-        response.setCharacterEncoding("utf-8");
-        return SUCCESS;
-    }
-
-    @Action(value = "/api/v1/student/create", results = { @Result(location = "/index.html") })
-    public String createStudent() throws IOException {
-        HttpServletResponse response = ServletActionContext.getResponse();
-        response.setCharacterEncoding("utf-8");
-        response.setContentType("application/json;charset=utf-8");
-        response.setHeader("Cache-Control", "no-cache");
-        PrintWriter pw = response.getWriter();
-        if (!isValid()) {
-            response.setStatus(400);
-            pw.print("{\"message\":\"Vui lòng nhập đầy đủ thông tin\"}");
-            pw.flush();
-            pw.close();
-            return SUCCESS;
-        }
-
+    @Action(value = "/CreateStudent", results = { @Result(location = "/index.html") })
+	public String createStudent() throws Exception{
+        System.out.println("it runs");
         Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
         SqlSession session = sqlSessionFactory.openSession();
-
-        Gson gson = new Gson();
 
         // create student mapper
         StudentMapper studentMapper = session.getMapper(StudentMapper.class);
 
         // insert student
-        Student student = new Student(name, branch, percentage, phone, email);
-        // Student student = gson.fromJson(jsonStudent, Student.class);
-        // System.out.println(student.toString());
-        String json = gson.toJson(student);
-
-        pw.print(json);
-        pw.flush();
-        pw.close();
-
-        // System.out.println(json);
-        // System.out.println(student.toString());
+        Student student = new Student(getName(), getBranch(), getPercentage(), getPhone(), getEmail());
+        System.out.println(student.toString());
         studentMapper.insert(student);
         session.commit();
         System.out.println("insert sucessfully");
 
         // close session
         session.close();
+		
+		// TODO Auto-generated method stub
+		return SUCCESS;
+	}
 
-        return SUCCESS;
-    }
-
-    //
-
-    // mai làm tiếp đoạn update
-    // @Update("/api/v1/student/update/{id}")
-    @Action(value = "/api/v1/student/update", results = { @Result(location = "/index.html") })
-    public String updateStudent() throws IOException {
-        Gson gson = new Gson();
-        // System.out.println(getJsonStudent());
-        // create student mapper
-
-        // insert student
-        Student student = new Student(getName(), getBranch(), getPercentage(), getPhone(), getEmail());
-        // Student student = gson.fromJson(jsonStudent, Student.class);
-        String json = gson.toJson(student);
-
-        /*
-         * HttpServletRequest request = ServletActionContext.getRequest();
-         * System.out.println(request.getMethod());
-         */
-        HttpServletResponse response = ServletActionContext.getResponse();
-        response.setCharacterEncoding("utf-8");
-        response.setContentType("application/json;charset=utf-8");
-        response.setHeader("Cache-Control", "no-cache");
-        PrintWriter pw = response.getWriter();
-        pw.print(json);
-        pw.flush();
-        pw.close();
-
-        System.out.println(json);
-        System.out.println(student.toString());
-        // System.out.println(student.toString());
-        System.out.println("insert sucessfully");
-
-        // TODO Auto-generated method stub
-        return SUCCESS;
-    }
-
-    // đổi nhánh xong xài delete bị lỗi luôn
-    // @Delete("/api/v1/student/delete/{id}")
-    @Action(value = "/api/v1/student/delete/*", params = { "id", "{1}" }, results = {
-            @Result(location = "/index.html") })
-    public String deleteStudent() throws IOException {
-        System.out.println(getId());
-        Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-        SqlSession session = sqlSessionFactory.openSession();
-
-        StudentMapper studentMapper = session.getMapper(StudentMapper.class);
-        studentMapper.delete(getId());
-        session.commit();
-        session.close();
-        return SUCCESS;
-    }
+	
+	
 }
--- src/main/webapp/js/student.js
+++ src/main/webapp/js/student.js
@@ -1,30 +1,13 @@
 const formDOM = document.querySelector('#create-student-form');
-const testBtnDOM = document.querySelector('#test-btn');
-const errorMsgDOM = document.querySelector('#error-msg');
 
 formDOM.addEventListener('submit', async (event) => {
     event.preventDefault();
-    const formData = new FormData(formDOM);
-    errorMsgDOM.textContent = '';
-    try {
-        const { data } = await axios.post('../api/v1/student/create', formData);
-        window.location.href = './index';
-        console.log(data);
-        //console.log('adadad');
-    } catch (error) {
-        errorMsgDOM.textContent = error.response.data.message;
-        //console.log(error.response.data.message);
-    }
-});
+    const data = new FormData(formDOM);
 
-testBtnDOM.addEventListener('click', async (event) => {
-    const formData = new FormData(formDOM);
-    /* const json = JSON.stringify(Object.fromEntries(formData));
-    const jsonData = new FormData();
-    jsonData.append('jsonStudent', json); */
     try {
-        const { data } = await axios.post('../api/v1/student/update', formData);
-        console.log(data);
+        await axios.post('CreateStudent', data);
+        window.location.href = './';
+        //console.log('adadad');
     } catch (error) {
         console.log(error);
     }
