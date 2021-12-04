53a42e203d5d528626545e8df382af259113e1f5
Ho Hieu Luc <kingofika123@gmail.com>
2021-12-04 19:42:50 +0700

    Cập nhật ví dụ về edit

--- src/main/webapp/js/student-browser.js
+++ src/main/webapp/js/student-browser.js
@@ -1,5 +1,4 @@
 const studentListDOM = document.querySelector('#student-list');
-const debugBtnDOM = document.querySelector('#debug-btn');
 
 const showStudentList = async () => {
     studentListDOM.textContent = 'Loading...';
@@ -18,7 +17,7 @@ const showStudentList = async () => {
                     <td>${email}</td>
                     <td>
                         <div class="d-flex justify-content-evenly">
-                            <a href="" class="">Sửa</a>
+                            <a href="./edit/${id}" class="">Sửa</a>
                             <a class="delete-btn" data-id="${id}">Xóa</a>
                         </div>
                     </td>
@@ -38,17 +37,10 @@ studentListDOM.addEventListener('click', async (event) => {
     if (eventTarget.classList.contains('delete-btn')) {
         const id = eventTarget.dataset.id;
         try {
-            studentListDOM.textContent = 'Loading...';
-            // đổi nhánh xong xài delete bị lỗi luôn phải chuyển sang xài post
-            //await axios.delete(`../api/v1/student/delete/${id}`);
             await axios.post(`../api/v1/student/delete/${id}`);
             showStudentList();
         } catch (error) {         
             console.log(error);
         }
     }
-});
-
-debugBtnDOM.addEventListener('click', () => {
-    showStudentList();
 });
