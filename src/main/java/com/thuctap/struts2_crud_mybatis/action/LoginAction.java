a1f16066face5918d8c3f7137e926523907d1608
Ho Hieu Luc <kingofika123@gmail.com>
2021-12-12 20:26:02 +0700

    Rút gọn code và chỉnh lại đường dẫn cho trang login và register

--- src/main/java/com/thuctap/struts2_crud_mybatis/action/LoginAction.java
+++ src/main/java/com/thuctap/struts2_crud_mybatis/action/LoginAction.java
@@ -1,7 +1,6 @@
 package com.thuctap.struts2_crud_mybatis.action;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,7 +31,8 @@ public class LoginAction extends ActionSupport {
     HttpSession session = request.getSession();
     SqlSessionFactory sqlSessionFactory = ConnectDB.getSqlSessionFactory();
 
-    private String username, password;
+    private String username;
+    private String password;
 
     public String getUsername() {
         return username;
@@ -51,57 +51,52 @@ public class LoginAction extends ActionSupport {
     }
 
     @Action(value = "/login", results = {
-            @Result(name = "notLoggedIn", location = "/login.html"),
-            @Result(name = "loggedIn", type = "redirect", location = "/welcome"),
+            @Result(name = "notLoggedIn", location = "/WEB-INF/jsp/login.jsp"),
+            @Result(name = "loggedIn", type = "redirect", location = "/"),
     })
     public String viewLogin() {
         Boolean loggedIn = (Boolean) session.getAttribute("loggedIn");
-        System.out.println("hmmmm");
         if (loggedIn == null || !loggedIn) {
             return "notLoggedIn";
         } else {
-            // nếu đã đăng nhập rồi thì sẽ tự chuyển sang trang welcome
-            // localhost:8080/.../login => welcome
+            // nếu đã đăng nhập rồi thì sẽ tự chuyển sang trang index
+            // localhost:8080/.../login => index
             return "loggedIn";
         }
     }
 
     @Action(value = "/loginAction", results = {
-            @Result(name = "loggedIn", type = "redirect", location = "/welcome"),
-            @Result(name = "success", location = "/login.html"),
+            @Result(name = "loggedIn", type = "redirect", location = "/"),
+            @Result(name = "success", location = "/WEB-INF/jsp/login.jsp"),
     })
     public String login() throws IOException {
         Boolean loggedIn = (Boolean) session.getAttribute("loggedIn");
-        // nếu đã đăng nhập rồi thì sẽ tự chuyển sang trang welcome
-        // localhost:8080/.../loginAction => welcome
+        // nếu đã đăng nhập rồi thì sẽ tự chuyển sang trang index
+        // localhost:8080/.../loginAction => index
         if (loggedIn != null && loggedIn) {
             return "loggedIn";
         }
 
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        PrintWriter printWriter = response.getWriter();
         AccountMapper accountMapper = sqlSession.getMapper(AccountMapper.class);
         Account account = accountMapper.getByUsername(username);
 
         //System.out.println("yes");
         if (account != null) {
             if (BCrypt.checkpw(password, account.getPassword())) {
-                System.out.println("Login success");
                 session.setAttribute("loggedIn", true);
                 session.setAttribute("username", username);
                 return "loggedIn";
             }
         }
         sqlSession.close();
-
-        return CustomError.createCustomError("Sai tài khoản hoặc mật khẩu", 401, response, printWriter);
+        return CustomError.createCustomError("Sai tài khoản hoặc mật khẩu", 401, response);
     }
 
     @Action(value = "/logout", results = {
-            @Result(name = "success", location = "/login", type = "redirect")
+            @Result(name = "success", location = "/", type = "redirect")
     })
     public String logout() {
-        System.out.println("logout");
         session.removeAttribute("loggedIn");
         session.removeAttribute("userName");
         return SUCCESS;
