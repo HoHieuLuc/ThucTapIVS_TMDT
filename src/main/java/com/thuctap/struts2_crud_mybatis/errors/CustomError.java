ebcab0aaffa2aec6194d556217a1acabe0e7aaf9
Ho Hieu Luc <kingofika123@gmail.com>
2021-12-12 20:26:10 +0700

    Update CustomError.java

--- src/main/java/com/thuctap/struts2_crud_mybatis/errors/CustomError.java
+++ src/main/java/com/thuctap/struts2_crud_mybatis/errors/CustomError.java
@@ -12,10 +12,10 @@ public class CustomError {
     public static String createCustomError(
             String message,
             int errorCode,
-            HttpServletResponse response,
-            PrintWriter printWriter) throws IOException {
-        System.out.println("bruh");
+            HttpServletResponse response) throws IOException {
+        PrintWriter printWriter = response.getWriter();
         response.setStatus(errorCode);
+        response.setContentType("application/json;charset=utf-8");
         printWriter.print("{\"message\":\"" + message + "\"}");
         printWriter.flush();
         printWriter.close();
