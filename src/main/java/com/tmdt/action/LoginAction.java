0be762433f60be01824b96031ebe5f310dc6b9d4
Ho Hieu Luc <kingofika123@gmail.com>
2021-12-23 17:28:40 +0700

    Update LoginAction.java

--- src/main/java/com/tmdt/action/LoginAction.java
+++ src/main/java/com/tmdt/action/LoginAction.java
@@ -20,93 +20,88 @@ import com.tmdt.errors.CustomError;
 import mybatis.mapper.*;
 import com.tmdt.model.*;
 
-@InterceptorRef("loggedInStack")
-public class LoginAction extends ActionSupport{
-   // Respone hay dùng cho AJAX và JSON
-   HttpServletResponse response = ServletActionContext.getResponse();
-   HttpServletRequest request = ServletActionContext.getRequest();
-   HttpSession session = request.getSession();
-   SqlSessionFactory sqlSessionFactory = ConnectDB.getSqlSessionFactory();
+@Result(name = "input", location = "/index", type = "redirectAction", params = {
+        "namespace", "/",
+        "actionName", "bad-request"
+})
+public class LoginAction extends ActionSupport {
+    // Respone hay dùng cho AJAX và JSON
+    HttpServletResponse response = ServletActionContext.getResponse();
+    HttpServletRequest request = ServletActionContext.getRequest();
+    HttpSession session = request.getSession();
+    SqlSessionFactory sqlSessionFactory = ConnectDB.getSqlSessionFactory();
 
-   private String username;
-   private String password;
+    private String username;
+    private String password;
 
-   public String getUsername() {
-       return username;
-   }
+    public String getUsername() {
+        return username;
+    }
 
-   public void setUsername(String username) {
-       this.username = username;
-   }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-   public String getPassword() {
-       return password;
-   }
+    public String getPassword() {
+        return password;
+    }
 
-   public void setPassword(String password) {
-       this.password = password;
-   }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-   @Action(value = "/login", results = {
-           @Result(name = "notLoggedIn", location = "/WEB-INF/jsp/login.jsp"),
-           @Result(name = "loggedIn", type = "redirect", location = "/"),
-   })
-   public String viewLogin() {
-       Boolean loggedIn = (Boolean) session.getAttribute("loggedIn");
-       if (loggedIn == null || !loggedIn) {
-           return "notLoggedIn";
-       } else {
-           // nếu đã đăng nhập rồi thì sẽ tự chuyển sang trang index
-           // localhost:8080/.../login => index
-           return "loggedIn";
-       }
-   }
+    @Action(value = "/login", results = {
+            @Result(name = "success", location = "/WEB-INF/jsp/login.jsp"),
+    }, interceptorRefs = {
+            @InterceptorRef("loggedInStack")
+    })
+    public String viewLogin() {
+        return "success";
 
-   @Action(value = "/loginAction", results = {
-           @Result(name = "loggedIn", type = "redirect", location = "/"),
-           @Result(name = "success", location = "/WEB-INF/jsp/login.jsp"),
-   })
-   public String login() throws IOException {
-       Boolean loggedIn = (Boolean) session.getAttribute("loggedIn");
-       // nếu đã đăng nhập rồi thì sẽ tự chuyển sang trang index
-       // localhost:8080/.../loginAction => index
-       if (loggedIn != null && loggedIn) {
-           return "loggedIn";
-       }
+    }
 
-       SqlSession sqlSession = sqlSessionFactory.openSession();
-       //Mapper lấy tài khoản
-       TaiKhoanMapper taiKhoanMapper = sqlSession.getMapper(TaiKhoanMapper.class);
-       //Mapper lấy thông tin khách Hàng
-       KhachHangMapper khachHangMapper = sqlSession.getMapper(KhachHangMapper.class);
-       
-       TaiKhoan account = taiKhoanMapper.getByUsername(username);
+    @Action(value = "/loginAction", results = {
+            @Result(name = "loggedIn", type = "redirect", location = "/"),
+            @Result(name = "success", location = "/WEB-INF/jsp/login.jsp"),
+    }, interceptorRefs = {
+            @InterceptorRef("loggedInStack")
+    })
+    public String login() throws IOException {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // Mapper lấy tài khoản
+        TaiKhoanMapper taiKhoanMapper = sqlSession.getMapper(TaiKhoanMapper.class);
+        // Mapper lấy thông tin khách Hàng
+        KhachHangMapper khachHangMapper = sqlSession.getMapper(KhachHangMapper.class);
 
-       if (account != null) {
-           //Kiểm tra mật khẩu và thời hạn tài khoản
-           if (BCrypt.checkpw(password, account.getPassword())) {
-               session.setAttribute("loggedIn", true);
-               session.setAttribute("permission",account.getMaQuyen());
-               session.setAttribute("username", username);
-               //lấy mã khách hàng bỏ vào trong session
-               session.setAttribute("maKhachHang",khachHangMapper.getMaKh(account.getId()));
-               System.out.println("Bạn đã đăng nhập account với quyền là " + session.getAttribute("permission"));
-               return "loggedIn";
-           }
-       }
-       sqlSession.close();
-       //Thông báo sai tài khoản
-       return CustomError.createCustomError("Tài khoản sai tên đăng nhập hoặc mật khẩu", 401, response);
-       
-   }
+        TaiKhoan account = taiKhoanMapper.getByUsername(username);
+        if (account != null) {
+            // Kiểm tra mật khẩu và thời hạn tài khoản
+            if (BCrypt.checkpw(password, account.getPassword())) {
+                System.out.println("bababooey");
+                session.setAttribute("loggedIn", true);
+                session.setAttribute("permission", account.getMaQuyen());
+                session.setAttribute("username", username);
+                // lấy mã khách hàng bỏ vào trong session
+                session.setAttribute("maKhachHang", khachHangMapper.getMaKh(account.getId()));
+                System.out.println("Bạn đã đăng nhập account với quyền là " + session.getAttribute("permission"));
+                return "loggedIn";
+            }
+        }
+        sqlSession.close();
+        // Thông báo sai tài khoản
+        return CustomError.createCustomError("Tài khoản sai tên đăng nhập hoặc mật khẩu", 401, response);
 
-   @Action(value = "/logout", results = {
-           @Result(name = "success", location = "/", type = "redirect")
-   })
-   public String logout() {
-       session.removeAttribute("loggedIn");
-       session.removeAttribute("userName");
-       return SUCCESS;
-   }
-    
+    }
+
+    @Action(value = "/logout", results = {
+            @Result(name = "success", location = "/", type = "redirect")
+    }, interceptorRefs = {
+            @InterceptorRef("authStack")
+    })
+    public String logout() {
+        session.removeAttribute("loggedIn");
+        session.removeAttribute("userName");
+        return SUCCESS;
+    }
+
 }
