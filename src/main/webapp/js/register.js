4ee833c79c467f06732d32b65985571695af8eb0
Ho Hieu Luc <kingofika123@gmail.com>
2021-12-23 17:02:03 +0700

    Update register.js

--- src/main/webapp/js/register.js
+++ src/main/webapp/js/register.js
@@ -1,6 +1,12 @@
 const formDOM = document.querySelector('#registerForm');
 const userNameErrorMessage = document.querySelector('#username_error');
 const passwordErrorMessage = document.querySelector('#password_error');
+const emailErrorMessage = document.querySelector('#email_error');
+const tenErrorMessage = document.querySelector('#ten_error');
+const facebookErrorMessage = document.querySelector('#facebook_error');
+const twitterErrorMessage = document.querySelector('#twitter_error');
+const retypePasswordErrorMessage = document.querySelector('#retype_password')
+const phoneErrorMessage = document.querySelector('#phone_error')
 
 const validateRegisterForm = async () => {
     const formData = new FormData(formDOM);
@@ -13,6 +19,13 @@ const validateRegisterForm = async () => {
         console.log(data);
         userNameErrorMessage.textContent = data.username ?? "";
         passwordErrorMessage.textContent = data.password ?? "";
+        emailErrorMessage.textContent = data.email ?? "";
+        tenErrorMessage.textContent = data.ten ?? "";
+        facebookErrorMessage.textContent = data.facebook_link ?? "";
+        twitterErrorMessage.textContent = data.twitter_link ?? "";
+        retypePasswordErrorMessage.textContent = data.xac_nhan_password ?? "";
+        phoneErrorMessage.textContent = data.dien_thoai ?? "";
+
     }
 }
 
