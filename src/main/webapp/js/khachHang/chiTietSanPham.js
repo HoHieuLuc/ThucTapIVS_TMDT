b4a054cfe98c3ec83a67ea055e6591079d3a6c3b
Ho Hieu Luc <kingofika123@gmail.com>
2021-12-25 08:00:02 +0700

    Update chiTietSanPham.js

--- src/main/webapp/js/khachHang/chiTietSanPham.js
+++ src/main/webapp/js/khachHang/chiTietSanPham.js
@@ -1,14 +1,13 @@
 //Lấy mã sản phẩm trên đường dẫn
 const params = window.location.pathname.split('/').slice(0);
 const maSanPham = params[params.length - 1];
-console.log(params);
-console.log(maSanPham);
 
 //Các biến của chi tiết sản phẩm
-const tenSanPham = document.querySelector('#tenSanPham');
-const danhGia = document.querySelector('#danhGia');
-const moTaSanPham = document.querySelector('#moTaSanPham');
-const gia = document.querySelector('#gia');
+const tenSanPhamDOM = document.querySelector('#tenSanPham');
+const danhGiaDOM = document.querySelector('#danhGia');
+const moTaSanPhamDOM = document.querySelector('#moTaSanPham');
+const giaDOM = document.querySelector('#gia');
+const anhChinhDOM = document.querySelector('#anhChinh');
 
 
 //Các biến của đánh giá sản phẩm
@@ -16,17 +15,33 @@ const danhGiaSPListDom = document.querySelector('#danhGiaSPListDom');
 const maSP_hiddenDom = document.querySelector('#maSP_hiddenDom');
 
 
+const showSanPhamDetail = async () => {
+    try {
+        const { data: { sanpham } } = await axios.get(`${baseURL}api/v1/sanpham/${maSanPham}`);
+        const { tenSanPham, moTa, gia, anhSanPham, xepHang } = sanpham;
+        tenSanPhamDOM.innerHTML = tenSanPham;
+        danhGiaDOM.innerHTML = xepHang ?? "Chưa có đánh giá";
+        moTaSanPhamDOM.innerHTML = moTa;
+        giaDOM.innerHTML = gia;
+        anhChinhDOM.src = `${baseURL}images/product/${anhSanPham}`;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+showSanPhamDetail();
+
 const showDanhGiaSPList = async () => {
     try {
-        const { data: danhGiaSPs } = await axios.get(`${baseURL}danhGiaSanPham/${maSanPham}`);
+        const { data: { danhGiaSPs } } = await axios.get(`${baseURL}danhGiaSanPham/${maSanPham}`);
         console.log(danhGiaSPs);
-        const allDanhGiaSPs = danhGiaSPs.danhSachDanhGia.map((danhGiaSP) => {
+        const allDanhGiaSPs = danhGiaSPs.map((danhGiaSP) => {
             const { ngay_sua, noi_dung, so_sao, ten } = danhGiaSP;
             //in ra icon ngôi sao đánh giá
             var so_sao_html = ` `;
-            for (let i = 0; i <so_sao; i++) {
-                 so_sao_html = `<span>&#9733;</span>` + so_sao_html;
-              } 
+            for (let i = 0; i < so_sao; i++) {
+                so_sao_html = `<span>&#9733;</span>` + so_sao_html;
+            }
             return `
             <div class="comment mt-4 text-justify float-left"> <img src="https://i.imgur.com/yTFUilP.jpg"
                     alt="avatar" class="rounded-circle" width="40" height="40">
@@ -48,15 +63,15 @@ const formDOM = document.querySelector('#formDanhGiaSanPham');
 
 const submitDanhGiaSP = async () => {
     const formData = new FormData(formDOM);
-    formData.append("maSanPham",maSanPham);
+    formData.append("maSanPham", maSanPham);
     //Thực hiện request
     try {
         await axios.post(`../danhGiaSanPhamSubmit`, formData);
         // Display the key/value pairs of form data
-            for (var pair of formData.entries()) {
-                console.log(pair[0]+ ', ' + pair[1]); 
-            }
-       // window.location.href = "./";  
+        for (var pair of formData.entries()) {
+            console.log(pair[0] + ', ' + pair[1]);
+        }
+        // window.location.href = "./";  
     } catch (error) {
         const data = error.response.data;
         console.log(data);
@@ -69,22 +84,4 @@ formDOM.addEventListener('submit', (event) => {
     submitDanhGiaSP();
 });
 
-
-
-
-
-const showSanPhamDetail = async () => {
-    try {
-        const { data: sanpham} = await axios.get(`${baseURL}api/v1/sanpham/details/${maSanPham}`);
-        console.log(sanpham);
-        tenSanPham.innerHTML = sanpham.tenSanPham;
-        danhGia.innerHTML = sanpham.xepHang;
-        moTaSanPham.innerHTML = sanpham.moTa;
-        gia.innerHTML = sanpham.gia;
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-showSanPhamDetail();
 
