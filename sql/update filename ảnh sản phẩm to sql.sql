978c4007c20209976d19ca350fcec6adada738bf
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2021-12-27 09:04:41 +0700

    tạo table log danh_gia_san_pham, tạo 2 file sql để sửa tên ảnh demo cho avatar ảnh sản phẩm

---
+++ sql/update filename ảnh sản phẩm to sql.sql
@@ -0,0 +1,204 @@
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '1.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 1;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '2.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 2;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '3.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 3;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '4.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 4;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '5.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 5;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '6.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 6;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '7.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 7;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '8.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 8;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '1.jfif'
+WHERE
+  `anh_san_pham`.`ma_anh` = 9;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '2.jfif'
+WHERE
+  `anh_san_pham`.`ma_anh` = 10;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '3.jfif'
+WHERE
+  `anh_san_pham`.`ma_anh` = 11;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '9.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 12;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '10.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 13;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '11.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 14;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '12.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 15;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '13.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 16;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '14.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 17;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '15.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 18;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '16.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 19;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '17.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 20;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '18.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 21;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '19.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 22;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '20.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 23;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '21.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 24;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '22.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 25;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '30.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 26;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '29.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 27;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '28.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 28;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '27.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 29;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '31.jfif'
+WHERE
+  `anh_san_pham`.`ma_anh` = 30;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '26.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 31;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '25.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 32;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '24.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 33;
+UPDATE
+  `anh_san_pham`
+SET
+  `anh` = '23.png'
+WHERE
+  `anh_san_pham`.`ma_anh` = 34;
