ba30e8df110fa8c4f159d2c79dd7086b27ffc75d
Ho Hieu Luc <kingofika123@gmail.com>
2021-12-27 19:16:24 +0700

    Update thongbao.js

--- src/main/webapp/js/thongbao.js
+++ src/main/webapp/js/thongbao.js
@@ -1,7 +1,15 @@
 const toastThongBao = document.querySelector('#toastThongBao');
+const toastHeader = document.querySelector('#toastHeader');
 const toastNoiDung = document.querySelector('#toastNoiDung');
+
 const toast = new bootstrap.Toast(toastThongBao);
-const thongBao = (msg) => {
+const thongBao = (msg, isError = false) => {
+    toastHeader.classList.add('bg-primary');
+    toastHeader.classList.remove('bg-danger');
+    if(isError) {
+        toastHeader.classList.add('bg-danger');
+        toastHeader.classList.remove('bg-primary');
+    }
     toastNoiDung.textContent = msg;
     toast.show();
 }
