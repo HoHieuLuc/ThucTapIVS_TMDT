cc2d94d05209bbc4bbe9e573aa17474e6d28eb15
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2021-12-30 13:36:23 +0700

    test hiển thị loại sản phẩm

--- src/main/webapp/js/product/loaisanpham-browser.js
+++ src/main/webapp/js/product/loaisanpham-browser.js
@@ -1,10 +1,18 @@
 const loaiSPListDOM = document.querySelector('#danhMucSP');
-
+		loaiSPListDOM.textContent = "Loading..."
 const showLoaiSPList = async () => {
 
     try {
         const { data: { loaiSanPhams } } = await axios.get(`${baseURL}/api/v1/loaisanpham`);
-        console.log(loaiSanPhams);
+      //  console.log(loaiSanPhams);
+		const allLoaiSanPhams = loaiSanPhams.map((loaiSanPham) => {
+            const { maLoaiSanPham,tenLoaiSanPham } = loaiSanPham;
+            return `
+               <a class="dropdown-item" href="${baseURL}/api/v1/sanpham/${maLoaiSanPham}">${tenLoaiSanPham}</a>
+			   `;
+        }).join('');
+		
+		loaiSPListDOM.innerHTML = allLoaiSanPhams;
     } catch (error) {
         console.log(error);
     }
