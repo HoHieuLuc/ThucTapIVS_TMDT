906c9f0442fe42c6e2a221d09fc777ff3b9d245d
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2021-12-31 15:59:32 +0700

    chỉnh sửa DGKH Action, Mapper

---
+++ src/main/java/com/tmdt/action/DanhGiaKhachHangAction.java
@@ -0,0 +1,71 @@
+package com.tmdt.action;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.tmdt.db.ConnectDB;
+import com.tmdt.errors.CustomError;
+import com.tmdt.model.DanhGiaKhachHang;
+import com.tmdt.utilities.JsonResponse;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.convention.annotation.*;
+
+import mybatis.mapper.DanhGiaKhachHangMapper;
+
+public class DanhGiaKhachHangAction extends ActionSupport {
+    // Respone hay dùng cho AJAX và JSON
+    HttpServletResponse response = ServletActionContext.getResponse();
+    HttpServletRequest request = ServletActionContext.getRequest();
+    HttpSession session = request.getSession();
+    SqlSessionFactory sqlSessionFactory = ConnectDB.getSqlSessionFactory();
+    private Integer soSao;
+    private int maKHDuocDanhGia;
+
+    public Integer getSoSao() {
+        return soSao;
+    }
+
+    public void setSoSao(Integer soSao) {
+        this.soSao = soSao;
+    }
+
+    public int getMaKHDuocDanhGia() {
+        return maKHDuocDanhGia;
+    }
+
+    public void setMaKHDuocDanhGia(int maKHDuocDanhGia) {
+        this.maKHDuocDanhGia = maKHDuocDanhGia;
+    }
+
+    @Action(value = "/api/v1/danhgia/khachhang/submit", results = {
+            @Result(name = SUCCESS, location = "/index.html")
+    }, interceptorRefs = {
+            @InterceptorRef(value = "khachHangStack"),
+    })
+    public String danhGiaKHSubmit() throws IOException {
+        if (soSao == null || soSao < 0 || soSao > 5) {
+            return CustomError.createCustomError("Đánh giá thất bại ", 400, response);
+        }
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        DanhGiaKhachHangMapper DanhGiaKhachHangMapper = sqlSession.getMapper(DanhGiaKhachHangMapper.class);
+        // Mã khách hàng đánh giá
+        int maKHDanhGia = (int) session.getAttribute("maNguoiDung");
+        Map<String, Object> jsonObject = new HashMap<String, Object>();
+        // Tạo instance DanhGiaKhachHang
+        DanhGiaKhachHang dgkh = new DanhGiaKhachHang(maKHDanhGia, maKHDuocDanhGia, soSao);
+        DanhGiaKhachHangMapper.themDGKhachHang(dgkh);
+        sqlSession.commit();
+        sqlSession.close();
+        jsonObject.put("message", "Đánh giá sản phẩm thành công");
+        return JsonResponse.createJsonResponse(jsonObject, 201, response);
+    }
+}
--- src/main/java/mybatis/mapper/DanhGiaKhachHangMapper.java
+++ src/main/java/mybatis/mapper/DanhGiaKhachHangMapper.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Options;
 public interface DanhGiaKhachHangMapper {
     public final String THEM_DANH_GIA_KHACH_HANG = "INSERT INTO `danh_gia_khach_hang` " +
      "(`ma_danh_gia`, `ma_kh_danh_gia`, `ma_kh_duoc_danh_gia`, `so_sao`, `ngay_tao`, `ngay_sua`) " +
-     "VALUES (NULL, '9', '2', '5', '2021-12-31 09:34:19.000000', '2021-12-31 09:34:19.000000');";
+     "VALUES (NULL, #{maKHDanhGia}, #{maKHDuocDanhGia}, #{soSao}, now(), null);";
      @Insert(THEM_DANH_GIA_KHACH_HANG)
      @Options(useGeneratedKeys = true,keyProperty = "maDanhGia")
      public void themDGKhachHang(DanhGiaKhachHang dgkh);
