7574bdfea3b24ddf31db6dad30d806d7e6533aad
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2021-12-31 15:42:22 +0700

    update KhachHangMapper

--- src/main/java/mybatis/mapper/DanhGiaKhachHangMapper.java
+++ src/main/java/mybatis/mapper/DanhGiaKhachHangMapper.java
@@ -1,8 +1,16 @@
 package mybatis.mapper;
 
+import com.tmdt.model.DanhGiaKhachHang;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+
 public interface DanhGiaKhachHangMapper {
     public final String THEM_DANH_GIA_KHACH_HANG = "INSERT INTO `danh_gia_khach_hang` " +
      "(`ma_danh_gia`, `ma_kh_danh_gia`, `ma_kh_duoc_danh_gia`, `so_sao`, `ngay_tao`, `ngay_sua`) " +
      "VALUES (NULL, '9', '2', '5', '2021-12-31 09:34:19.000000', '2021-12-31 09:34:19.000000');";
+     @Insert(THEM_DANH_GIA_KHACH_HANG)
+     @Options(useGeneratedKeys = true,keyProperty = "maDanhGia")
+     public void themDGKhachHang(DanhGiaKhachHang dgkh);
      
 }
