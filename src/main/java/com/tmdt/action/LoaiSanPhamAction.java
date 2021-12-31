98fd899ab7d07cb37d8849cddad5e6ec9ec8eee7
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2021-12-31 09:53:25 +0700

    ah hiểu rồi, né tên /loaisanpham ra

--- src/main/java/com/tmdt/action/LoaiSanPhamAction.java
+++ src/main/java/com/tmdt/action/LoaiSanPhamAction.java
@@ -54,7 +54,7 @@ public class LoaiSanPhamAction extends ActionSupport {
         return JsonResponse.createJsonResponse(map, 200, response);
     }
 
-    @Action(value = "/api/v1/loaisanpham/*",params = {"{maLoaiSanPham}","{1}"} , results = {
+    @Action(value = "/api/v1/listSanPhamByLSP/{maLoaiSanPham}", results = {
             @Result(name = "success", location = "/index.html")
     })
     public String getAllSanPhamByLSP() throws IOException {
