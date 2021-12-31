2420a9e05eaecff576923dfc32ebc5bd2be26c89
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2021-12-31 07:13:06 +0700

    Update store-browser.js

--- src/main/webapp/js/store/store-browser.js
+++ src/main/webapp/js/store/store-browser.js
@@ -6,10 +6,10 @@ const showstoreListDOM = async () => {
         const { data: { listStores } } = await axios.get(`${baseURL}/api/v1/store`);
         //{ma_khach_hang: 1, avatar: "1.png", ten: "Lâm Minh Thiện"}
 		const alllistStores = listStores.map((listStore) => {
-            const { ma_khach_hang,avatar,ten } = listStore;
+            const { avatar,ten,username } = listStore;
             return `
                <div class="col-sm-3">
-                    <a href="${baseURL}/store/${ma_khach_hang}">
+                    <a href="${baseURL}/store/${username}">
                             <img src="${baseURL}images/user/${avatar}" class="img-responsive" style="width: 200px; height: 200px; border-radius: 30%;" alt="${ten}">
                     </a>
                      <p>${ten}</p>
