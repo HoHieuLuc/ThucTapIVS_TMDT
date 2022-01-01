adec3168ea368e6aad019136e7327f0df3f91d26
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-01 17:39:33 +0700

    fix GioHangMapper

--- src/main/java/mybatis/mapper/GioHangMapper.java
+++ src/main/java/mybatis/mapper/GioHangMapper.java
@@ -22,13 +22,16 @@ public interface GioHangMapper {
       public void themSP_GioHang(@Param("maKhachHang") int maKhachHang,@Param("maSanPham") String maSanPham);
 
       //cập nhật số lượng sản phẩm trong giỏ hàng
-      final String UPDATE_SO_LUONG_SP = "";
+      final String UPDATE_SO_LUONG_SP = "UPDATE `gio_hang` SET `ma_khach_hang` = #{maKhachHang}, `ma_san_pham` = #{maSanPham}, " +
+      "`so_luong` = #{soLuong} " +
+      "WHERE `gio_hang`.`ma_khach_hang` = #{maKhachHang} AND `gio_hang`.`ma_san_pham` = #{maSanPham};";
       @Update(UPDATE_SO_LUONG_SP)
       public void updateSoLuongSP(@Param("maKhachHang") int maKhachHang,@Param("maSanPham") String maSanPham,@Param("soLuong") int soLuong);
 
 
       //Xóa sản phẩm ra khỏi giỏ hàng
-      final String DELETE_SP_GIO_HANG = "";
+      final String DELETE_SP_GIO_HANG = "DELETE FROM `gio_hang` WHERE `gio_hang`.`ma_khach_hang` = #{maKhachHang} " +
+      "AND `gio_hang`.`ma_san_pham` = #{maSanPham}";
       @Delete(DELETE_SP_GIO_HANG)
       public void deleteSP(@Param("maKhachHang") int maKhachHang,@Param("maSanPham") String maSanPham);
 }
