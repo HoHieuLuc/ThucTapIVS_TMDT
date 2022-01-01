70594e910018a8f7a9b310723861f647d2840014
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-01 11:24:21 +0700

    ghi chú cho giohang.js

--- src/main/webapp/js/product/giohang.js
+++ src/main/webapp/js/product/giohang.js
@@ -1,11 +1,11 @@
-//Hàm này sẽ kiểm tra xem sản phẩm đó có trong giỏ hàng (khách hàng session) hay chưa
+//Hàm này sẽ kiểm tra xem sản phẩm đó có trong giỏ hàng database (tham chiếu đến khách hàng session bên backend) hay chưa
 //Và sẽ cập nhật lên các button sản phẩm từ đặt hàng thành, Đã thêm vào giỏ hàng
 const capNhatUIGioHang = ()=>{
 
 }
 
 const themVaoGioHang = (maSanPham) => {
-    /*TODO */
+    /*TODO: Thêm vào server ngay khi người dùng click chuột (liệu có lag server mysql không) */
     document.querySelector(`#button2_${maSanPham}`).innerText = "Đã thêm vào giỏ hàng"
     alert("Bạn đã thêm sản phẩm có mã là:" + maSanPham);
     // Gọi lại hàm cập nhật UI giỏ hàng
