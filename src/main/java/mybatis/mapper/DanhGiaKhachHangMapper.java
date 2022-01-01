153caa009d786ad05230578cbb51ca754de9f612
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-01 14:35:51 +0700

    update DanhGiaKHMapper

--- src/main/java/mybatis/mapper/DanhGiaKhachHangMapper.java
+++ src/main/java/mybatis/mapper/DanhGiaKhachHangMapper.java
@@ -1,16 +1,27 @@
 package mybatis.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import com.tmdt.model.DanhGiaKhachHang;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
 public interface DanhGiaKhachHangMapper {
     public final String THEM_DANH_GIA_KHACH_HANG = "INSERT INTO `danh_gia_khach_hang` " +
-     "(`ma_danh_gia`, `ma_kh_danh_gia`, `ma_kh_duoc_danh_gia`, `so_sao`, `ngay_tao`, `ngay_sua`) " +
-     "VALUES (NULL, #{maKHDanhGia}, #{maKHDuocDanhGia}, #{soSao}, now(), null);";
-     @Insert(THEM_DANH_GIA_KHACH_HANG)
-     @Options(useGeneratedKeys = true,keyProperty = "maDanhGia")
-     public void themDGKhachHang(DanhGiaKhachHang dgkh);
-     
+            "(`ma_danh_gia`, `ma_kh_danh_gia`, `ma_kh_duoc_danh_gia`, `so_sao`, `ngay_tao`, `ngay_sua`) " +
+            "VALUES (NULL, #{maKHDanhGia}, #{maKHDuocDanhGia}, #{soSao}, now(), null);";
+
+    @Insert(THEM_DANH_GIA_KHACH_HANG)
+    @Options(useGeneratedKeys = true, keyProperty = "maDanhGia")
+    public void themDGKhachHang(DanhGiaKhachHang dgkh);
+
+    // Kiểm tra khách hàng đã đánh giá
+    public final String KIEM_TRA_KH_DA_DANH_GIA = "SELECT * FROM `danh_gia_khach_hang` " +
+            "WHERE ma_kh_danh_gia = #{maKHDanhGia} and ma_kh_duoc_danh_gia = #{maKHDuocDanhGia};";
+    @Select(KIEM_TRA_KH_DA_DANH_GIA)
+    public List<Map<String,Object>> kiemTraDanhGia();
+
 }
