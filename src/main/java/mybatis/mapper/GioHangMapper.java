8f400877a45d8e7ebcdf19477e576989af238f62
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-01 16:57:07 +0700

    backup GioHangMapper 1

--- src/main/java/mybatis/mapper/GioHangMapper.java
+++ src/main/java/mybatis/mapper/GioHangMapper.java
@@ -1,9 +1,11 @@
 package mybatis.mapper;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 public interface GioHangMapper {
     // Hàm lấy giỏ hàng của khách đang đăng nhập
@@ -18,4 +20,15 @@ public interface GioHangMapper {
       "VALUES (#{maKhacHang},#{maSanPham},'1')";
       @Insert(THEM_SP_VAO_GIO_HANG)
       public void themSP_GioHang(@Param("maKhachHang") int maKhachHang,@Param("maSanPham") String maSanPham);
+
+      //cập nhật số lượng sản phẩm trong giỏ hàng
+      final String UPDATE_SO_LUONG_SP = "";
+      @Update(UPDATE_SO_LUONG_SP)
+      public void updateSoLuongSP(@Param("maKhachHang") int maKhachHang,@Param("maSanPham") String maSanPham,@Param("soLuong") int soLuong);
+
+
+      //Xóa sản phẩm ra khỏi giỏ hàng
+      final String DELETE_SP_GIO_HANG = "";
+      @Delete(DELETE_SP_GIO_HANG)
+      public void deleteSP(@Param("maKhachHang") int maKhachHang,@Param("maSanPham") String maSanPham);
 }
