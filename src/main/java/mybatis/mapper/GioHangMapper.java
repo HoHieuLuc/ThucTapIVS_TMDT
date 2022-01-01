fd4ee22befd32c81d5403f4c538d3abb3a152829
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-01 14:30:35 +0700

    update giỏ hàng mapper

--- src/main/java/mybatis/mapper/GioHangMapper.java
+++ src/main/java/mybatis/mapper/GioHangMapper.java
@@ -1,5 +1,8 @@
 package mybatis.mapper;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 public interface GioHangMapper {
@@ -9,4 +12,10 @@ public interface GioHangMapper {
      " ON kh.ma_khach_hang = gh.ma_khach_hang WHERE kh.ma_khach_hang = #{maKhachHang} ";  
      @Select(GET_GIO_HANG_BY_MA_KH) 
      public void getGioHangByMaKH(int maKhachHang);
+
+     //Hàm thêm sản phẩm vào giỏ hàng
+      final String THEM_SP_VAO_GIO_HANG =  "INSERT INTO `gio_hang` (`ma_khach_hang`, `ma_san_pham`, `so_luong`) " +
+      "VALUES (#{maKhacHang},#{maSanPham},'1')";
+      @Insert(THEM_SP_VAO_GIO_HANG)
+      public void themSP_GioHang(@Param("maKhachHang") int maKhachHang,@Param("maSanPham") String maSanPham);
 }
