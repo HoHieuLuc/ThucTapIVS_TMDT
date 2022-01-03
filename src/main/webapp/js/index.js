45170639b973e2c4d15115b15b998323b2b6ef0f
Ho Hieu Luc <kingofika123@gmail.com>
2022-01-03 20:18:29 +0700

    Update index.js

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,17 +1,19 @@
 const loginLinkDOM = document.querySelector('#login');
 const registerLinkDOM = document.querySelector('#register');
 const logOutLinkDOM = document.querySelector('#logout');
-const loginDanhGiaKhachHangDOM = document.querySelector('#loginDanhGiaKH');
+const loginDanhGiaDOM = document.querySelector('#loginDanhGia');
 const currentURL = new URL(window.location);
 currentURL.searchParams.delete('redirect');
 
 if (loginLinkDOM) {
     loginLinkDOM.href = `${baseURL}login?redirect=${encodeURIComponent(currentURL)}`;
     registerLinkDOM.href = `${baseURL}register?redirect=${encodeURIComponent(currentURL)}`;
-    loginDanhGiaKhachHangDOM.href = `${baseURL}login?redirect=${encodeURIComponent(currentURL)}`;
+    if (loginDanhGiaDOM) {
+        loginDanhGiaDOM.href = `${baseURL}login?redirect=${encodeURIComponent(currentURL)}`;
+    }
 }
 
-if(logOutLinkDOM){
+if (logOutLinkDOM) {
     logOutLinkDOM.addEventListener('click', async (event) => {
         event.preventDefault();
         await axios.post(`${baseURL}logout`);
