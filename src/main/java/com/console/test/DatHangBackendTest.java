30b815592a783281b91fd6ed9cc13a64c4020e4e
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-03 16:25:24 +0700

    updateDatHangMapper

--- src/main/java/com/console/test/DatHangBackendTest.java
+++ src/main/java/com/console/test/DatHangBackendTest.java
@@ -1,5 +1,8 @@
 package com.console.test;
 
+import java.util.List;
+import java.util.Map;
+
 import com.tmdt.db.ConnectDB;
 
 import org.apache.ibatis.exceptions.PersistenceException;
@@ -11,9 +14,7 @@ import mybatis.mapper.DatHangMapper;
 public class DatHangBackendTest {
     //Getter and setter Simulator
     /*
-        @Param("maKhachHang") String maKhachHang,
-        @Param("tongTien") int tongTien,
-        @Param("tinhTrang") int tinhTrang,
+      @Param("maDatHang") int maDatHang,
         @Param("maSanPham") String maSanPham,
         @Param("soLuong") int soLuong
     */
@@ -25,12 +26,15 @@ public class DatHangBackendTest {
 
          // Lấy mã khách hàng từ session
          Integer maKhachHang = 1;
-         
+         // Get giỏ hàng (lấy danh sách gồm mã sản phẩm và số lượng tương ứng cho từng sản phẩm đó)
+         List<Map<String, Object>> gioHangByMaKHs = datHangMapper.getGioHangByMaKH(maKhachHang);
          try {
             datHangMapper.themDonDHMoi(maKhachHang);
-            // Khi sai mã sản phẩm ở câu querry thứ 2  
-            // Thì câu querry thứ nhất không commit được dữ liệu vào database
-            sqlSession.commit();
+            for (Map<String,Object> gh : gioHangByMaKHs) {
+                System.out.println(gh.get("so_luong"));
+                
+            }
+            //sqlSession.commit();
          }
          catch(PersistenceException e) {
             System.out.println(e.getMessage());
--- src/main/java/mybatis/mapper/DatHangMapper.java
+++ src/main/java/mybatis/mapper/DatHangMapper.java
@@ -1,15 +1,40 @@
 package mybatis.mapper;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Param;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.*;
+
 
 public interface DatHangMapper {
     //To obtain the value immediately after an INSERT, use a SELECT query with the LAST_INSERT_ID() function.
     //Tham khảo từ (https://dev.mysql.com/)
     final String THEM_DON_DH_MOI = "INSERT INTO `dat_hang`(`ma_dat_hang`, `ma_khach_hang`, `ngay_dat`, `tong_tien`, `tinh_trang`) "+ 
-    "VALUES (NULL,#{maKhachHang},now(),(select sum(gh.so_luong*sp.gia) from gio_hang gh join san_pham sp on sp.ma_san_pham = gh.ma_san_pham where gh.ma_khach_hang = 1),0)";
+    "VALUES (NULL,#{maKhachHang},now(),(select sum(gh.so_luong*sp.gia) from gio_hang gh join san_pham sp " +
+    "on sp.ma_san_pham = gh.ma_san_pham where gh.ma_khach_hang = #{maKhachHang}),0)";
     @Insert(THEM_DON_DH_MOI)
     public void themDonDHMoi(
+        @Param("maKhachHang") int maKhachHang
+    );
+
+    //Lấy mã đặt hàng gần đây nhất của khách hàng để làm chi tiết đặt hàng
+    final String GET_MA_DH_LATELY = "SELECT ma_dat_hang from dat_hang WHERE ma_khach_hang = #{maKhachHang} ORDER BY ma_dat_hang DESC limit 1;";
+    @Select(GET_MA_DH_LATELY)
+    public int getMaDHLatELY(int maKhachHang);
+
+    //Thêm chi tiết đặt hàng
+    final String THEM_CHI_TIET_DH ="INSERT INTO `chi_tiet_dat_hang`(`ma_dat_hang`, `ma_san_pham`, `so_luong`) VALUES (#{maDatHang},#{maSanPham},#{soLuong})";
+    @Insert(THEM_CHI_TIET_DH)
+    public void themChiTietDh(
+        @Param("maDatHang") int maDatHang,
+        @Param("maSanPham") String maSanPham,
+        @Param("soLuong") int soLuong
+    );
+
+    //Lấy thông tin của giỏ hàng để làm chi tiết đặt hàng
+    final String GET_GIO_HANG_BY_MA_KH = "select so_luong,ma_san_pham from gio_hang where ma_khach_hang = #{maKhachHang};";
+    @Select(GET_GIO_HANG_BY_MA_KH)
+    public List<Map<String, Object>> getGioHangByMaKH(
         @Param("maKhachHang") int maKhachHang
     );
 
