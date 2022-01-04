1d2c6998959eeb9360b1a7563d9453aa6e06cc89
Ho Hieu Luc <kingofika123@gmail.com>
2022-01-04 15:16:01 +0700

    Update index.js

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -2,6 +2,9 @@ const loginLinkDOM = document.querySelector('#login');
 const registerLinkDOM = document.querySelector('#register');
 const logOutLinkDOM = document.querySelector('#logout');
 const loginDanhGiaDOM = document.querySelector('#loginDanhGia');
+
+const bodyDOM = document.querySelector('body');
+
 const currentURL = new URL(window.location);
 currentURL.searchParams.delete('redirect');
 
@@ -20,3 +23,53 @@ if (logOutLinkDOM) {
         location.reload();
     })
 }
+
+const addToCart = async (formData) => {
+    try {
+        await axios.post(`${baseURL}api/v1/giohang/them`, formData);
+        thongBao('Thêm vào giỏ hàng thành công');
+    } catch (error) {
+        thongBao(error.response.data.message ?? 'Có lỗi xảy ra', true);
+    }
+}
+
+const removeFromCart = async (formData) => {
+    try {
+        await axios.post(`${baseURL}api/v1/giohang/xoa`, formData);
+        thongBao('Xóa khỏi giỏ hàng thành công');
+    } catch (error) {
+        thongBao(error.response.data.message ?? 'Có lỗi xảy ra', true);
+    }
+}
+
+const updateCart = async (formData) => {
+    try {
+        await axios.post(`${baseURL}api/v1/giohang/capnhat`, formData);
+    } catch (error) {
+        thongBao(error.response.data.message ?? 'Có lỗi xảy ra', true);
+    }
+}
+
+
+bodyDOM.addEventListener('click', async (event) => {
+    const target = event.target;
+    const formData = new FormData();
+    if (target.classList.contains('add-to-cart-btn')) {
+        formData.append('maSanPham', target.dataset.masanpham);
+        await addToCart(formData);
+        return;
+    }
+    if (target.classList.contains('remove-from-cart-btn')) {
+        formData.append('maSanPham', target.dataset.masanpham);
+        await removeFromCart(formData);
+        return;
+    }
+    if (target.classList.contains('update-cart-btn')) {
+        //await updateCart(target.dataset.masanpham, target.previousElementSibling.value);
+        console.log("update cart");
+        return;
+    }
+    if (target.classList.contains('add-to-fav-btn')) {
+        console.log("add to favorite");
+    }
+});
