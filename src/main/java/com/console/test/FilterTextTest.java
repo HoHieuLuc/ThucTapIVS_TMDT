be552654d8780358ad68a4ead54cd0078368922b
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-05 17:05:37 +0700

    Thêm hàm đọc file để test badWords

--- src/main/java/com/console/test/FilterTextTest.java
+++ src/main/java/com/console/test/FilterTextTest.java
@@ -1,21 +1,36 @@
 package com.console.test;
 
-public class FilterTextTest {
-    
-        public static void main(String[] args) {
-          String noiDung = "H e l l      o Tui là thiện";
-          //Trim  những khoản trắng ở giữa, chỉ để kiểm tra, chứ không phải
-          //lưu vào database
-          String noiDung_Trim = noiDung.replace(" ","");
-          //Lowwer toàn bộ kí tự cần kiểm tra xuống chữ thường
-          noiDung_Trim.toLowerCase();
-          
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-          
-         // System.out.println(myStr.toUpperCase().("b"));
-        //   System.out.println(myStr.contains("e"));
-        //   System.out.println(myStr.contains("Hi"));
+public class FilterTextTest {
+        //Hàm đọc file và chuyển từng dòng thành arrayList
+        public static ArrayList<Integer> readFileToArray(String filename) throws FileNotFoundException
+        {
+                ArrayList<Integer> result = new ArrayList<>();
+
+                try (Scanner s = new Scanner(new FileReader(filename))) {
+                        while (s.hasNext()) {
+                                result.add(s.nextInt());
+                        }
+                        return result;
+                }
         }
-      
-      
+
+        public static void main(String[] args) throws FileNotFoundException {
+                String noiDung = "H e l l      o Tui là thiện";
+                // Trim những khoản trắng ở giữa, chỉ để kiểm tra, chứ không phải
+                // lưu vào database
+                String noiDung_Trim = noiDung.replace(" ", "");
+                // Lowwer toàn bộ kí tự cần kiểm tra xuống chữ thường
+                noiDung_Trim.toLowerCase();
+
+                 // Đọc file badWord.txt và đưa vào array
+                ArrayList<Integer> badWords = readFileToArray("filename");
+                
+                
+        }
+
 }
