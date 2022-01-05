f330279d712ea02d50f99479323a128a3a41b121
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-05 16:52:46 +0700

    create badWord and update filterText

--- src/main/java/com/console/test/FilterTextTest.java
+++ src/main/java/com/console/test/FilterTextTest.java
@@ -3,10 +3,15 @@ package com.console.test;
 public class FilterTextTest {
     
         public static void main(String[] args) {
-          String myStr = "H e l l      o Tui là thiện";
-          //Trim  những khoản trắng ở giữa
-          System.out.println(myStr.replace("",""));
-          //Upper toàn bộ kí tự cần kiểm tralên in hoa
+          String noiDung = "H e l l      o Tui là thiện";
+          //Trim  những khoản trắng ở giữa, chỉ để kiểm tra, chứ không phải
+          //lưu vào database
+          String noiDung_Trim = noiDung.replace(" ","");
+          //Lowwer toàn bộ kí tự cần kiểm tra xuống chữ thường
+          noiDung_Trim.toLowerCase();
+          
+
+          
          // System.out.println(myStr.toUpperCase().("b"));
         //   System.out.println(myStr.contains("e"));
         //   System.out.println(myStr.contains("Hi"));
