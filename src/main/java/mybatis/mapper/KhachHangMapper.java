605a34ffe78f925127b8d97f51574d97bd79ab79
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-05 09:07:33 +0700

    Tránh hiển thị  "Nhân viên " lên Trang danh sách cửa hàng

--- src/main/java/mybatis/mapper/KhachHangMapper.java
+++ src/main/java/mybatis/mapper/KhachHangMapper.java
@@ -52,9 +52,10 @@ public interface KhachHangMapper {
     public List<Map<Integer, Integer>> getProductRating(String username);
 
     // Lấy avatar, mã khách hàng, tên khách hàng để làm trang danh sách store
-    final String GET_LIST_STORE = "SELECT TK.avatar,TK.username,KH.ten from khach_hang " +
-            "kh LEFT JOIN san_pham sp on kh.ma_khach_hang = sp.ma_khach_hang " +
-            " LEFT JOIN tai_khoan tk on kh.id_tai_khoan = tk.id GROUP BY username;";
+    final String GET_LIST_STORE = " SELECT TK.avatar,TK.username,KH.ten from khach_hang kh " +
+        "LEFT JOIN tai_khoan tk on kh.id_tai_khoan = tk.id " +
+        "WHERE tk.id NOT  IN (SELECT id_tai_khoan FROM nhan_vien) " +
+        "GROUP BY username; ";
 
     @Select(GET_LIST_STORE)
     public List<Map<String, Object>> getListStore();
