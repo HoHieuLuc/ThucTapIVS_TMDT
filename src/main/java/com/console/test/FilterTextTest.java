2e31de1eef1960b3399db38d2af9bfce6f80cbc6
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-05 16:44:58 +0700

    Test, filter

---
+++ src/main/java/com/console/test/FilterTextTest.java
@@ -0,0 +1,16 @@
+package com.console.test;
+
+public class FilterTextTest {
+    
+        public static void main(String[] args) {
+          String myStr = "H e l l      o Tui là thiện";
+          //Trim  những khoản trắng ở giữa
+          System.out.println(myStr.replace("",""));
+          //Upper toàn bộ kí tự cần kiểm tralên in hoa
+         // System.out.println(myStr.toUpperCase().("b"));
+        //   System.out.println(myStr.contains("e"));
+        //   System.out.println(myStr.contains("Hi"));
+        }
+      
+      
+}
