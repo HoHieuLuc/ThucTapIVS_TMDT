f2218b82a857773898f37ee96877c5ef59235e3c
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-06 15:59:50 +0700

    thêm nút, gắn sự kiện, lấy dữ liệu vào console để test Trang duyệt sản phẩm

--- src/main/webapp/WEB-INF/jsp/admin/pages/kiemduyet/sanpham/index.jsp
+++ src/main/webapp/WEB-INF/jsp/admin/pages/kiemduyet/sanpham/index.jsp
@@ -2,12 +2,18 @@
 <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <jsp:include page="/WEB-INF/jsp/admin/include/header.jsp" />
 
-<div class="btn-group" role="group" aria-label="Bộ lọc các nút xem danh sách sản phẩm">
-  <button type="button" class="btn btn-danger">Đã Xóa</button>
-  <button type="button" class="btn btn-primary">Trong kho</button>
-  <button type="button" class="btn btn-warning">Chờ duyệt</button>
-  <button type="button" class="btn btn-success">Đã duyệt</button>
-</div>
+    <!-- Các button tương tác với trang web -->
+    <div class="btn-group" role="group" aria-label="Bộ lọc các nút xem danh sách sản phẩm">
+    <button type="button" class="btn btn-danger" id="biXoa">Đã Xóa</button>
+    <button type="button" class="btn btn-primary" id="trongKho">Trong kho</button>
+    <button type="button" class="btn btn-warning" id="yeuCauDuyet">Chờ duyệt</button>
+    <button type="button" class="btn btn-success" id="daDuyet">Đã duyệt</button>
+    </div>
+
+
+    <div id="listSanPham">
+        Đang tải dữ liệu
+    </div>
 
 
    <script src='<c:url value="/js/admin/kiemduyet/listsanpham_bystatus.js"/>'></script>
--- src/main/webapp/js/admin/kiemduyet/listsanpham_bystatus.js
+++ src/main/webapp/js/admin/kiemduyet/listsanpham_bystatus.js
@@ -1,15 +1,44 @@
+//Đổi tiêu đề trang web
+document.title = "Trang phê duyệt sản phẩm "
+
 //Nút hiển thị sản phẩm mới vô kho,yêu cầu kiểm duyệt,đã duyệt, và bị xóa (hide)
 const listTrongKhoDOM = document.querySelector("#trongKho");
 const listYeuCauDuyetDOM = document.querySelector("#yeuCauDuyet");
-const listDaDuyetKhoDOM = document.querySelector("#DaDuyet");
+const listDaDuyetDOM = document.querySelector("#daDuyet");
 const listBiXoaDOM = document.querySelector("#biXoa");
 //Khu vực danh sách sản phẩm thẻ <div>
 const listSanPhamDOM = document.querySelector("#listSanPham");
 
-//Render lại danh sách sản phẩm đúng với status 
+//Render lại danh sách sản phẩm đúng với status  
 listTrongKhoDOM.addEventListener('click', async () => {
+    document.title = "Sản phẩm trong kho "
     const {data : {sanphams} } = await axios.post(`${baseURL}api/v1/sanpham/getbystatus/0`);
     console.log(sanphams);
-    userNameErrorMessage.innerHTML = "<p>Test thử nút listTrongKho</p>";
+    listSanPhamDOM.innerHTML = "<p>Test thử nút listTrongKho</p>";
 })
+
+//Render lại danh sách sản phẩm đúng với status 
+listBiXoaDOM.addEventListener('click', async () => {
+    document.title = "Sản phẩm trong kho "
+    const {data : {sanphams} } = await axios.post(`${baseURL}api/v1/sanpham/getbystatus/1`);
+    console.log(sanphams);
+    listSanPhamDOM.innerHTML = "<p>Test thử nút listBiXoa</p>";
+})
+
+//Render lại danh sách sản phẩm đúng với status 
+listDaDuyetDOM.addEventListener('click', async () => {
+    document.title = "Sản phẩm trong kho "
+    const {data : {sanphams} } = await axios.post(`${baseURL}api/v1/sanpham/getbystatus/2`);
+    console.log(sanphams);
+    listSanPhamDOM.innerHTML = "<p>Test thử nút listDaDuyet</p>";
+})
+
+//Render lại danh sách sản phẩm đúng với status 
+listYeuCauDuyetDOM.addEventListener('click', async () => {
+    document.title = "Sản phẩm trong kho "
+    const {data : {sanphams} } = await axios.post(`${baseURL}api/v1/sanpham/getbystatus/-1`);
+    console.log(sanphams);
+    listSanPhamDOM.innerHTML = "<p>Test thử nút listYeuCauDuyet</p>";
+})
+
 
