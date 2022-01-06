3f2f86fc1641519f5293e1fbe98773a6839eb760
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-06 19:55:17 +0700

    dùng lại focusout cho check username trùng

--- src/main/webapp/js/register.js
+++ src/main/webapp/js/register.js
@@ -35,7 +35,7 @@ const register = async () => {
 }
 
 //Kiểm tra username bị trùng
-    usernameDOM.addEventListener('keyup', async () => {
+    usernameDOM.addEventListener('focusout', async () => {
         const {data : {message} } = await axios.post(`${baseURL}checkUsername?username=${usernameDOM.value}`);
         userNameErrorMessage.textContent = message;
     })
