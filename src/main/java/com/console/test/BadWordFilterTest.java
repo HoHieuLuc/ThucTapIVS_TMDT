3f74665957bbf785be1aa17b46a14c243dbfca4a
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-06 08:29:53 +0700

    yes, bắt được tiếng việt rồi

--- src/main/java/com/console/test/BadWordFilterTest.java
+++ src/main/java/com/console/test/BadWordFilterTest.java
@@ -14,14 +14,9 @@ import com.tmdt.utilities.ProjectPath;
 public class BadWordFilterTest {
 	
 	public static void main(String[] args) throws InterruptedException, FileNotFoundException, UnsupportedEncodingException {
-		String input="vếu aaa";
-		input.getBytes(Charset.forName("utf-8"));
-		String hi = new String("vếu aa".getBytes(Charset.forName("utf-8")));
-		//input = new String(input.getBytes("UTF-8"), "ISO-1");
-		//String output = BadWordFilter.getCensoredText(input);
-		//System.out.println(output);
-		System.out.println(input);
-		System.out.println("Test");
+		String input = new String("vếu aa".getBytes(Charset.forName("utf-8")));
+		String output = BadWordFilter.getCensoredText(input);
+		System.out.println(output);
 
 	}
 
