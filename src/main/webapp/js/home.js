0581701cc0d66e7bb06d8c5ec6a7d53cdc1bdc4c
Ho Hieu Luc <kingofika123@gmail.com>
2022-01-08 22:37:33 +0700

    file js cho trang chủ

---
+++ src/main/webapp/js/home.js
@@ -0,0 +1,83 @@
+const sanPhamDaXemDOM = document.querySelector('#sanPhamDaXem');
+const loaiSanPhamPhoBienDOM = document.querySelector('#loaiSanPhamPhoBien');
+const storePhoBienDOM = document.querySelector('#storeDuocDanhGiaCao');
+const sanPhamMoiDOM = document.querySelector('#sanPhamMoi');
+
+const showLoaiSanPhamPhoBien = async () => {
+    try {
+        const { data: { popularCategory } } = await axios.get(`${baseURL}api/v1/category/popular`);
+        const allPopularCategory = popularCategory.map((category) => {
+            const { ma_loai_sp, ten_loai_sp, anh } = category;
+            return `
+                <div class="col-2">
+                    <a href="${baseURL}category/${ma_loai_sp}" 
+                        class="text-decoration-none text-dark text-center" 
+                    >
+                        <img src="${baseURL}images/category/${anh}" class="tlt-thumbnail rounded mx-auto d-block" alt="${ten_loai_sp}">
+                        <div>${ten_loai_sp}</div>
+                    </a>
+                </div>
+            `;
+        }).join('');
+        loaiSanPhamPhoBienDOM.innerHTML = allPopularCategory;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const showTopStore = async () => {
+    try {
+        const { data: { topStores } } = await axios.get(`${baseURL}api/v1/store/top`);
+        const allTopStores = topStores.map((store) => {
+            const { username, ten, avatar, xep_hang } = store;
+            return `
+                <div class="col-2">
+                    <a href="${baseURL}store/${username}" 
+                        class="text-decoration-none text-dark text-center" 
+                    >
+                        <img src="${baseURL}images/user/${avatar}" class="tlt-thumbnail rounded mx-auto d-block" alt="${ten}">
+                        <div>${ten}</div>
+                        <div>${xep_hang} &#9733;</div>
+                    </a>
+                </div>
+            `;
+        }).join('');
+        storePhoBienDOM.innerHTML = allTopStores;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const showNewestProduct = async () => {
+    try {
+        const { data: { sanphams } } = await axios.get(`${baseURL}api/v1/sanpham/new`);
+        const allNewestProduct = sanphams.map((sanpham) => {
+            const { ma_san_pham, ten_san_pham, gia, anh } = sanpham;
+            const giaVND = gia.toLocaleString("vi-VN", {
+                style: "currency",
+                currency: "VND",
+            });
+            return `
+                <div class="col-2">
+                    <a href="${baseURL}sanpham/${ma_san_pham}" 
+                        class="text-decoration-none text-dark text-center" 
+                    >
+                        <img src="${baseURL}images/product/${anh}" class="tlt-thumbnail rounded mx-auto d-block" alt="${ten_san_pham}">
+                        <div>${ten_san_pham}</div>
+                        <div>${giaVND}</div>
+                    </a>
+                    <button type="button" data-masanpham="${ma_san_pham}" class="add-to-cart-btn w-100 btn btn-block btn-warning">
+                        <i class="fas fa-cart-plus"></i>
+                    </button>
+                </div>
+            `;
+        }).join('');
+        sanPhamMoiDOM.innerHTML = allNewestProduct;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+showLoaiSanPhamPhoBien();
+showTopStore();
+showNewestProduct();
