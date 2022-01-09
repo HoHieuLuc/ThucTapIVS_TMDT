96a2920d33cccda497fc4eced3b67a95ae77c7c5
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-09 08:56:55 +0700

    Update BaoCaoNguoiDungAction.java

--- src/main/java/com/tmdt/action/BaoCaoNguoiDungAction.java
+++ src/main/java/com/tmdt/action/BaoCaoNguoiDungAction.java
@@ -1,14 +1,9 @@
 package com.tmdt.action;
 
 import com.opensymphony.xwork2.ActionSupport;
-
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.tmdt.db.ConnectDB;
 import com.tmdt.errors.CustomError;
-import com.tmdt.utilities.JsonResponse;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,10 +23,11 @@ public class BaoCaoNguoiDungAction extends ActionSupport {
     HttpServletRequest request = ServletActionContext.getRequest();
     HttpSession session = request.getSession();
     SqlSessionFactory sqlSessionFactory = ConnectDB.getSqlSessionFactory();
-    
 
     private String userName;
     private String noiDung;
+    private int maBaoCao;
+    private int status;
 
     /* Region Getter and setter */
     public String getUserName() {
@@ -50,16 +46,33 @@ public class BaoCaoNguoiDungAction extends ActionSupport {
         this.noiDung = noiDung;
     }
 
+    public int getMaBaoCao() {
+        return maBaoCao;
+    }
+
+    public void setMaBaoCao(int maBaoCao) {
+        this.maBaoCao = maBaoCao;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    /* End Region Getter and setter */
+
     @Action(value = "/api/v1/baocao/{userName}/submit", results = {
-        @Result(name = SUCCESS, location = "/index.html")
-        }, interceptorRefs = {
-                @InterceptorRef(value = "khachHangStack"),
-        })
-    public String baoCaoNguoi() throws IOException {
+            @Result(name = SUCCESS, location = "/index.html")
+    }, interceptorRefs = {
+            @InterceptorRef(value = "khachHangStack"),
+    })
+    public String baoCaoNguoiDung() throws IOException {
         System.out.println("Username bị báo cáo là " + userName);
         System.out.println("Nội dung báo cáo là " + noiDung);
-        
-        //Lấy id người gửi
+
+        // Lấy id người gửi
         int idNguoiGui = (int) session.getAttribute("accountID");
 
         // Lấy id người nhận
@@ -68,28 +81,51 @@ public class BaoCaoNguoiDungAction extends ActionSupport {
         TaiKhoanMapper taiKhoanMapper = sqlSession.getMapper(TaiKhoanMapper.class);
         int idNguoiNhan = taiKhoanMapper.getTaiKhoanIdByUsername(userName);
 
-        
-        //Chặn không cho mình tự báo cáo chính mình
-        if (idNguoiGui == idNguoiNhan)
-        {
+        // Chặn không cho mình tự báo cáo chính mình
+        if (idNguoiGui == idNguoiNhan) {
             sqlSession.close();
-            return CustomError.createCustomError("Bạn không thể tự báo cáo chính mình",403,response);
+            return CustomError.createCustomError("Bạn không thể tự báo cáo chính mình", 403, response);
         }
 
-        //Chặn khi nội dung quá ngắn
-        if (noiDung.length() <20 || noiDung.length() > 255)
-        {
+        // Chặn khi nội dung quá ngắn
+        if (noiDung.length() < 20 || noiDung.length() > 255) {
             sqlSession.close();
-            return CustomError.createCustomError("Nội dung phải từ 20 ký tự trở lên",403,response);
+            return CustomError.createCustomError("Nội dung phải từ 20 ký tự trở lên", 403, response);
         }
 
-        //Tiến hành gửi báo cáo
-        baoCaoNguoiDungMapper.guiBaoCaoNguoiDung(idNguoiNhan, idNguoiGui,noiDung);
+        // Tiến hành gửi báo cáo
+        baoCaoNguoiDungMapper.guiBaoCaoNguoiDung(idNguoiNhan, idNguoiGui, noiDung);
         sqlSession.commit();
         sqlSession.close();
         return SUCCESS;
     }
 
+    // api/v1/nhanvien/baocao/changestatus?maBaoCao=${ma_bao_cao}&status=${status}
+    @Action(value = "/api/v1/nhanvien/baocao/changestatus", results = {
+            @Result(name = SUCCESS, location = "/index.html")
+    }, interceptorRefs = {
+            @InterceptorRef(value = "khachHangStack"),
+    })
+    public String duyetBaoCaoNguoiDung() throws IOException {
 
+        // Lấy id người nhận
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BaoCaoNguoiDungMapper baoCaoNguoiDungMapper = sqlSession.getMapper(BaoCaoNguoiDungMapper.class);
+        TaiKhoanMapper taiKhoanMapper = sqlSession.getMapper(TaiKhoanMapper.class);
+        int idNguoiNhan = taiKhoanMapper.getTaiKhoanIdByUsername(userName);
 
+        // Duyệt báo cáo đó
+        baoCaoNguoiDungMapper.updateBaoCaoStatus(status, maBaoCao);
+
+        // Tăng số lần cảnh cáo lên 1 và đồng thời gửi thông báo cho người bị vi phạm
+        if (status == 1)
+        {
+            baoCaoNguoiDungMapper.tangSoLanCanhBao(idNguoiNhan);
+        }
+
+        sqlSession.commit();
+        sqlSession.close();
+        return SUCCESS;
+
+    }
 }
