d3db00942c51f9febb168d1eb325bfec52df4305
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-10 18:12:41 +0700

    Update ThongBaoMapper.java

--- src/main/java/mybatis/mapper/ThongBaoMapper.java
+++ src/main/java/mybatis/mapper/ThongBaoMapper.java
@@ -4,7 +4,7 @@ import org.apache.ibatis.annotations.*;
 
 public interface ThongBaoMapper {
     final String TAO_THONG_BAO = "INSERT INTO `thong_bao` (`ma_tb`, `id_nguoi_nhan`, `id_nguoi_gui`, `noi_dung`, `ngay_tao`, `status`) " +
-                "VALUES (NULL, #{idNguoiNhan}, #{idNguoiGui}, #{noiDung}, now(), '1'); ";
+                "VALUES (NULL, #{idNguoiNhan}, #{idNguoiGui}, #{noiDung}, now(), 0); ";
     @Insert(TAO_THONG_BAO)
     public void taoThongBao(
         @Param("idNguoiNhan") int idNguoiNhan,
