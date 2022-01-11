1e22d4747aab3889454f20a0174be9751b1b5e28
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-11 20:16:24 +0700

    thêm thông báo cho admin

--- src/main/webapp/WEB-INF/jsp/admin/include/footer.jsp
+++ src/main/webapp/WEB-INF/jsp/admin/include/footer.jsp
@@ -19,5 +19,7 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <!-- AdminLTE App -->
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/js/adminlte.min.js"></script>
+<!-- Script xử lý thông báo -->
+<script src='<c:url value="/js/admin/dashboard/thongbao.js"/>'></script>
 </body>
 </html>
---
+++ src/main/webapp/js/admin/dashboard/thongbao.js
@@ -0,0 +1,82 @@
+const listThongBaoDOM = document.querySelector("#listThongBao");
+const soThongBaoDOM = document.querySelector("#soThongBao");
+
+const showThongBao = async (status) => {
+    try {
+        const { data: { thong_baos } } = await axios.get(`${baseURL}api/v1/thongbao/${status}`);
+        if (thong_baos.length === 0) {
+            listThongBaoDOM.innerHTML = `Không có thông báo nào`;
+            return;
+        }
+        const allThongBaos = thong_baos.map(data => {
+
+            const { noi_dung, nguoi_gui, ngay_tao, status } = data;
+
+            return `
+                <li class="list-group-item d-flex justify-content-between align-items-start ${status} dropdown-item">
+                    <div class="ms-2 me-auto">
+                    <div class="fw-bold">${nguoi_gui}</div>
+                        ${noi_dung}
+                    </div>
+                    <span class="badge bg-warning rounded-pill">
+                        ${ngay_tao.date.day}/${ngay_tao.date.month}/${ngay_tao.date.year} 
+                    </span>
+                </li>`;
+
+        }).join('');
+        listThongBaoDOM.innerHTML = allThongBaos;
+    }
+    catch (error) {
+        console.log(error);
+        //thongBao(error.response.data.message, true);
+    }
+}
+
+// Hiển thị số thông báo , status = 999
+const showSoThongBao = async () => {
+    try {
+        const { data: { chua_doc } } = await axios.get(`${baseURL}api/v1/thongbao/${999}`);
+        document.querySelector("#soThongBao").innerHTML = chua_doc;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+showSoThongBao();
+
+
+// Cho tạm số bất kì khác 0,999 để hiện tất cả thông báo
+showThongBao(-1);
+
+document.querySelector("#danhDauDaDoc").addEventListener('click', async () => {
+    try {
+        await axios.get(
+            `${baseURL}api/v1/thongbao/seenall`
+        )
+    } catch (error) {
+        console.log(error);
+        thongBao(error.response.data.message, true);
+    }
+    showThongBao(-1);
+    showSoThongBao();
+})
+
+//Mở list thông báo chưa đọc, status = 0
+document.querySelector("#listChuaDoc").addEventListener('click', () => {
+    showThongBao(0);
+})
+
+//Mở list tất cả thông báo 
+document.querySelector("#listAll").addEventListener('click', () => {
+    showThongBao(-1);
+})
+
+//Giữ cho dropdown menu không bị đóng khi nhấn vô mấy button
+$('body > div.wrapper > nav > ul.navbar-nav.ml-auto > li.nav-item.dropdown.me-2 > ul').on({
+    "click": function (e) {
+        e.stopPropagation();
+    }
+});
+
+
+
