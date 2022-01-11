9ee7e1ab2705731fb43a0e14072c4c48a5be99a4
Ho Hieu Luc <kingofika123@gmail.com>
2022-01-11 23:25:19 +0700

    Update SanPhamYeuThichMapper.java

--- src/main/java/mybatis/mapper/SanPhamYeuThichMapper.java
+++ src/main/java/mybatis/mapper/SanPhamYeuThichMapper.java
@@ -6,29 +6,28 @@ import java.util.Map;
 import org.apache.ibatis.annotations.*;
 
 public interface SanPhamYeuThichMapper {
-    //Thêm sản phẩm yêu thích
-    final String THEM_SP_YEU_THICH  = "INSERT INTO `san_pham_yeu_thich` (`ma_khach_hang`, `ma_san_pham`) " +
-        "VALUES (#{maKhachHang}, #{maSanPham});";
-    @Select(THEM_SP_YEU_THICH)
+    // Thêm sản phẩm yêu thích
+    final String THEM_SP_YEU_THICH = "INSERT INTO `san_pham_yeu_thich` (`ma_khach_hang`, `ma_san_pham`) " +
+            "VALUES (#{maKhachHang}, #{maSanPham});";
+
+    @Insert(THEM_SP_YEU_THICH)
     public int themSPYeuThich(
-        @Param("maKhachHang") int maKhachHang,
-        @Param("maSanPham") String maSanPham
+            @Param("maKhachHang") int maKhachHang,
+            @Param("maSanPham") String maSanPham);
 
-    );
-
-    //Lấy danh sách sản phẩm yêu thích
+    // Lấy danh sách sản phẩm yêu thích
     final String GET_SP_YEU_THICH = "SELECT * FROM `san_pham_yeu_thich` WHERE ma_khach_hang = #{maKhachHang};";
+
     @Select(GET_SP_YEU_THICH)
     public List<Map<String, Object>> getSPYeuThich(int maKhachHang);
 
-    
-    //Xóa 1 sản phẩm trong `san_pham_yeu_thich`
+    // Xóa 1 sản phẩm trong `san_pham_yeu_thich`
     final String XOA_SP_YEU_THICH = "DELETE FROM `san_pham_yeu_thich` " +
-    "WHERE `ma_khach_hang` = #{maKhachHang}  AND `ma_san_pham` = #{maSanPham};";
+            "WHERE `ma_khach_hang` = #{maKhachHang}  AND `ma_san_pham` = #{maSanPham};";
+
     @Delete(XOA_SP_YEU_THICH)
     public int xoaSPYeuThich(
-        @Param("maKhachHang") int maKhachHang,
-        @Param("maSanPham") String maSanPham
-    );
+            @Param("maKhachHang") int maKhachHang,
+            @Param("maSanPham") String maSanPham);
 
 }
