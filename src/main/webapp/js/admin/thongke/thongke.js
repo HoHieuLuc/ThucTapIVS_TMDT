c5a84e2a41a95a4fb863abcf766617eed45d97f5
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2022-01-12 21:17:20 +0700

    backup lại hàm axios

--- src/main/webapp/js/admin/thongke/thongke.js
+++ src/main/webapp/js/admin/thongke/thongke.js
@@ -2,32 +2,44 @@
 
 const thongKeTinhTrangDonHang = async () => {
 
-    const ctx = document.getElementById('pieChart').getContext('2d');
+  const ctx = document.getElementById('pieChart').getContext('2d');
 
-    //Dữ liệu
-    const data = {
-        labels: [
-          'Red',
-          'Blue',
-          'Yellow'
-        ],
-        datasets: [{
-          label: 'My First Dataset',
-          data: [300, 50, 100],
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-          ],
-          hoverOffset: 4
-        }]
-      };
+  //Dữ liệu
+  const data = {
+    labels: [
+      'Red',
+      'Blue',
+      'Yellow'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
 
-      //Cấu hình
-      const config = {
-        type: 'pie',
-        data: data,
-      };
-    const pieChart = new Chart(ctx,config);
+  //Cấu hình
+  const config = {
+    type: 'pie',
+    data: data,
+  };
+  const pieChart = new Chart(ctx, config);
 }
 thongKeTinhTrangDonHang();
+
+const thongKeDonGian = async () => {
+  try {
+    const { data: { thong_ke } } = await axios.get(`${baseURL}api/v1/nhanvien/thongke/0`);
+    console.log(thong_ke.so_thanh_vien);
+  }
+  catch (error) {
+    thongBao(error.response.data.message ?? 'Có lỗi xảy ra', true);
+  }
+}
+
+thongKeDonGian();
