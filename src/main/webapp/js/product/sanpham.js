96f1cf6c2f5094e0c4ec41c46db5c383439b712e
Ho Hieu Luc <kingofika123@gmail.com>
2022-01-13 22:22:17 +0700

    Cập nhật những người đặt mua sản phẩm này cũng đặt

--- src/main/webapp/js/product/sanpham.js
+++ src/main/webapp/js/product/sanpham.js
@@ -98,6 +98,7 @@ const showSanPhamDetail = async (skip = false) => {
         buildInlineGallery(anhSanPhamData).openGallery();
         showDanhGiaSPs(1);
         getSanPhamCuaShop(username, maSanPham, ma_loai_sp, ma_loai_cha);
+        getSanPhamGoiY(maSanPham);
     } catch (error) {
         console.log(error);
         thongBao(error.response.data.message, true);
--- src/main/webapp/js/product/suggested-product.js
+++ src/main/webapp/js/product/suggested-product.js
@@ -1,11 +1,10 @@
-const testSwiperDOM = document.querySelector('.sanPhamCungShopSwiper');
-const innerSwiperDOM = document.querySelector('.sanPhamCungShopWrapper');
+const sanPhamCungShopWrapperDOM = document.querySelector('.sanPhamCungShopWrapper');
 
 const getSanPhamCuaShop = async (username, maSanPham, maLoaiSanPham, maLoaiCha) => {
     try {
         const {
             data: { products },
-        } = await axios.get(`${baseURL}api/v1/store/${username}/products/suggested`, {
+        } = await axios.get(`${baseURL}api/v1/store/${username}/products/suggestion`, {
             params: {
                 maSanPham,
                 maLoaiSanPham,
@@ -47,7 +46,7 @@ const getSanPhamCuaShop = async (username, maSanPham, maLoaiSanPham, maLoaiCha) 
                 </div>
             `;
         }).join('');
-        innerSwiperDOM.innerHTML = allProducts;
+        sanPhamCungShopWrapperDOM.innerHTML = allProducts;
         new Swiper(".sanPhamCungShopSwiper", {
             slidesPerView: 3,
             spaceBetween: 3,
@@ -62,3 +61,65 @@ const getSanPhamCuaShop = async (username, maSanPham, maLoaiSanPham, maLoaiCha) 
         console.log(error);
     }
 };
+
+const sanPhamGoiYWrapperDOM = document.querySelector('.sanPhamGoiYWrapper');
+
+const getSanPhamGoiY = async(maSanPham) => {
+    try {
+        const {
+            data: { sanPhams },
+        } = await axios.get(`${baseURL}api/v1/sanpham/suggestion`, {
+            params: {
+                maSanPham,
+            },
+        });
+        if (sanPhams.length === 0) {
+            return;
+        }
+        const allProducts = sanPhams.map((product) => {
+            const { ma_san_pham, ten_san_pham, gia, anh } = product;
+            const giaVND = gia.toLocaleString("vi-VN", {
+                style: "currency",
+                currency: "VND",
+            });
+            return `
+                <div class="swiper-slide">
+                    <div>
+                        <a 
+                            href="${baseURL}sanpham/${ma_san_pham}"
+                            class="d-flex"    
+                        >
+                            <img 
+                                src="${baseURL}images/product/${anh}" 
+                                alt="${ten_san_pham}"
+                                class="img-fluid tlt-thumbnail mx-auto my-auto"
+                            >
+                        </a>
+                        <a 
+                            href="${baseURL}sanpham/${ma_san_pham}" 
+                            style="width: 15em"
+                            class="fs-5 text-decoration-none tlt-overflow-ellipsis text-dark"
+                            title="${ten_san_pham}"
+                        >
+                            ${ten_san_pham}
+                        </a>
+                        <div class="text-center">${giaVND}</div>
+                    </div>
+                </div>
+            `;
+        }).join('');
+        sanPhamGoiYWrapperDOM.innerHTML = allProducts;
+        new Swiper(".sanPhamGoiYSwiper", {
+            slidesPerView: 3,
+            spaceBetween: 3,
+            freeMode: true,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+        document.querySelector('.sanPhamGoiYDiv').classList.remove('d-none');
+    } catch (error) {
+        console.log(error);
+    }
+}
