86429092db40355e80888d5c5008cbec9e0b931d
Ho Hieu Luc <kingofika123@gmail.com>
2022-01-13 20:05:56 +0700

    cập nhật lại giao diện thông báo

--- src/main/webapp/js/listThongBao.js
+++ src/main/webapp/js/listThongBao.js
@@ -4,9 +4,9 @@ let danhDauDaDocCuThe;
 const thongbaoTableDOM = document.querySelector("#thongbao-table");
 const thongBaoFilterDOM = document.querySelector("#thongbao-filter");
 
-const showThongBao = async (status) => {
+const showThongBao = async (_status) => {
     try {
-        const { data: { thong_baos } } = await axios.get(`${baseURL}api/v1/thongbao/${status}`);
+        const { data: { thong_baos } } = await axios.get(`${baseURL}api/v1/thongbao/${_status}`);
         if (thong_baos.length === 0) {
             listThongBaoDOM.innerHTML = `Không có thông báo nào`;
             return;
@@ -16,20 +16,29 @@ const showThongBao = async (status) => {
 
             const { noi_dung, nguoi_gui, ngay_tao, status, ma_tb } = data;
             //Nếu phát hiện thông báo này chưa đọc, tạo nút đã đọc tương ứng
-            if (status.includes("bg-secondary") || status.includes("bg-body")) danhDauDaDocCuThe = `<button type="button" class="btn btn-primary"  data-id="${ma_tb}" >Đã đọc</button>`;
-            else danhDauDaDocCuThe = ``;
+            if (status.includes("bg-secondary") || status.includes("bg-body")) {
+                danhDauDaDocCuThe = `
+                    <button type="button" class="seen-btn btn btn-primary btn-sm rounded-circle" data-id="${ma_tb}">
+                        <i class="fas fa-check"></i>
+                    </button>
+                `;
+            }
+            else {
+                danhDauDaDocCuThe = ``;
+            }
             return `
                 <li class="list-group-item d-flex justify-content-between align-items-start ${status} dropdown-item">
                     <div class="ms-2 me-auto">
-                    <div class="fw-bold">${nguoi_gui}</div>
-                        ${noi_dung}
+                        <div class="fw-bold">${nguoi_gui}</div>
+                        <div style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; width:123px;">
+                            ${noi_dung}
+                        </div>
                     </div>
-                    ${danhDauDaDocCuThe}
                     <span class="badge bg-warning rounded-pill">
                         ${ngay_tao.date.day}/${ngay_tao.date.month}/${ngay_tao.date.year} 
                     </span>
+                    ${danhDauDaDocCuThe}
                 </li>`;
-
         }).join('');
         listThongBaoDOM.innerHTML = allThongBaos;
     }
@@ -55,51 +64,32 @@ showSoThongBao();
 // Cho tạm số bất kì khác 0,999 để hiện tất cả thông báo
 showThongBao(-1);
 
-document.querySelector("#danhDauDaDoc").addEventListener('click', async () => {
-    try {
-        // -9999 đánh dấu toàn bộ đã đọc
-        const { data: { message } } = await axios.get(`${baseURL}api/v1/thongbao/seen/-9999`);
-        thongBao(message, false);
-
-    } catch (error) {
-        console.log(error);
-    }
-    showThongBao(-1);
-    showSoThongBao();
-})
-
-
-
-
-//Giữ cho dropdown menu không bị đóng khi nhấn vô mấy button
-$('body > div.wrapper > nav > ul.navbar-nav.ml-auto > li.nav-item.dropdown.me-2 > ul').on({
-    "click": function (e) {
-        e.stopPropagation();
-    }
-});
+const thongBaoMenuDOM = document.querySelector("#thongBaoMenu");
 
 // Thêm sự kiện cập nhật đã đọc cho từng thông báo trong dropdown menu
-listThongBaoDOM.addEventListener('click', async (event) => {
+thongBaoMenuDOM.addEventListener('click', async (event) => {
     const target = event.target;
-    const id = target.dataset.id;
-    try {
-        const { data: { message } } = await axios.get(`${baseURL}api/v1/thongbao/seen/${id}`);
-        thongBao(message, false);
+    if (target.classList.contains('seen-btn')) {
+        const id = target.dataset.id;
+        try {
+            const { data: { message } } = await axios.get(`${baseURL}api/v1/thongbao/seen/${id}`);
+            thongBao(message, false);
 
-    } catch (error) {
-        console.log(error);
-        thongBao(error.response.data.message, true);
+        } catch (error) {
+            console.log(error);
+            thongBao(error.response.data.message, true);
+        }
+        showThongBao(-1);
+        showSoThongBao();
     }
-    showThongBao(-1);
-    showSoThongBao();
 })
 
 
 //Nếu phát hiện element có id="thongbao-table"
 if (thongbaoTableDOM) {
-    const showThongBaoTable = async (status) => {
+    const showThongBaoTable = async (_status) => {
         try {
-            const { data: { thong_baos } } = await axios.get(`${baseURL}api/v1/thongbao/${status}`);
+            const { data: { thong_baos } } = await axios.get(`${baseURL}api/v1/thongbao/${_status}`);
             if (thong_baos.length === 0) {
                 thongbaoTableDOM.innerHTML = `Không có thông báo nào`;
                 return;
@@ -110,15 +100,26 @@ if (thongbaoTableDOM) {
                 const { noi_dung, nguoi_gui, ngay_tao, status, ma_tb } = data;
 
                 //Nếu phát hiện thông báo này chưa đọc, tạo nút đã đọc tương ứng
-                if (status.includes("bg-secondary") || status.includes("bg-body")) danhDauDaDocCuThe = `<button type="button" class="btn btn-primary"  data-id="${ma_tb}" >Đã đọc</button>`;
-                else danhDauDaDocCuThe = ``;
+                if (status.includes("bg-secondary") || status.includes("bg-body")) {
+                    danhDauDaDocCuThe = `
+                        <button type="button" class="btn btn-outline-primary" data-id="${ma_tb}">
+                            <i class="fas fa-check"></i>
+                        </button>
+                    `;
+                } else {
+                    danhDauDaDocCuThe = ``;
+                }
 
                 return `
                     <tr>
-                            <td>${noi_dung}</td>
-                            <td>${nguoi_gui}</td>
-                            <td>${ngay_tao.date.day}/${ngay_tao.date.month}/${ngay_tao.date.year} </td>
-                            <td>${danhDauDaDocCuThe}</td>
+                        <td>${noi_dung}</td>
+                        <td>${nguoi_gui}</td>
+                        <td>${ngay_tao.date.day}/${ngay_tao.date.month}/${ngay_tao.date.year} </td>
+                        <td>
+                            <div class="d-flex justify-content-evenly">
+                                ${danhDauDaDocCuThe}
+                            </div>
+                        </td>
                     </tr>
                     `;
 
